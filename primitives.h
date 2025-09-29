3cb0cd97c5dfa6ea92e6d8ab4fbe28e1d65e11c3
Yonaim <yonaimyo@gmail.com>
2025-09-30 01:02:39 +0900

    WIP: primitives.h

---
+++ primitives.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/**
+ * @brief primitives - f, l, p
+ *
+ * face -> polygon mesh
+ * line -> polylines
+ * point -> point cloud
+ */
+
+#include <vector>
+
+struct Mesh {};
+struct Lines {};
+struct Points {};
